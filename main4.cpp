dff6fa357b47d6de683d60e979b9ccf3eb3e9323
Qoo <myungthe@naver.com>
2018-07-31 17:26:18 +0900

    문제해결을 위한 창의적 알고리즘 (중급) 171page 문제

---
+++ main4.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+int g[201][201], v[201] , n, m;
+
+void solve()
+{
+	list<int> list;
+
+	list.push_front(0);
+	while (!list.empty())
+	{
+		int current = list.front(); list.pop_front();
+		v[current] = 1;
+		for (auto i = list.begin(); i != list.end(); i++)
+		{
+			if (current == *i)
+			{
+				cout << "IMPOSSIBLE" << endl;
+				return;
+			}
+		}
+		for (size_t i = 0; i < n; i++)
+		{
+			if (!v[i] && g[current][i])
+				list.push_front(i);
+		}
+	}
+
+	cout << "OK" << endl;
+
+	return;
+}
+
+void output()
+{
+
+}
+
+void input()
+{
+	int s, e;
+
+	cin >> n >> m;
+	for (size_t i = 1; i <= m; i++)
+	{
+		cin >> s >> e;
+		g[s][e] = g[e][s] = 1;
+	}
+}
+
+int main()
+{
+	input();
+	solve();
+	output();
+
+	return 0;
+}
