6bf5d599303f3e22dff681ea10e341e7a7db66b3
Qoo <myungthe@naver.com>
2018-07-31 14:16:13 +0900

    문제해결을 위한 창의적 알고리즘 (중급) 156page 문제

---
+++ main2.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int r[11][11];
+int v[11];
+int n, min = 0x7FFFFFFF;
+
+void search(int a, int b, int s)
+{
+	v[b] = 1;
+	s += r[a][b];
+
+	if (a == n - 1)
+	{
+		if (s < min)
+			min = s;
+		return;
+	}
+
+	for (size_t i = 0; i < n; i++)
+	{
+		if (!v[i])
+			search(a + 1, i, s);
+	}
+
+	v[a] = 0;
+}
+
+void solve()
+{
+	for (size_t i = 0; i < n; i++)
+	{
+		search(0, i, 0);
+	}
+}
+
+void input()
+{
+	cin >> n;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			cin >> r[i][j];
+		}
+	}
+}
+
+void output()
+{
+	cout << min << endl;
+}
+
+int main()
+{
+	input();
+	solve();
+	output();
+
+	return 0;
+}
