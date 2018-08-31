54267a5d33e8d79d84d7f165666e8897e3635ffe
Qoo <myungthe@naver.com>
2018-08-31 10:17:33 +0900

    https://www.acmicpc.net/problem/1003 문제
    피보나치 수가 n-1과 n-2의 합이듯이 0과 1이 호출되는 횟수 또한 n-1과 n-2에서 호출되는 횟수와 같다.
    
    따라서 0과 1을 호출하는 횟수를 DP로 구현하여 풀었다.

---
+++ main54.cpp
@@ -0,0 +1,28 @@
+﻿/* https://www.acmicpc.net/problem/1003 Quiz */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int T;
+	int zero[50] = { 1, 0 };
+	int one[50] = { 0, 1 };
+
+	for (int j = 2; j < 50; j++)
+	{
+		zero[j] = zero[j - 1] + zero[j - 2];
+		one[j] = one[j - 1] + one[j - 2];
+	}
+
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		int n;
+		cin >> n;
+		cout << zero[n] << " " << one[n] << "\n";
+	}
+
+	return 0;
+}
